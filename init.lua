09d52da21925c7e3fa2c5c43cb86ab55aaa3d48f
TZWwww <tttzw@stu.pku.edu.cn>
2024-11-07 11:18:04 +0000

    u

--- init.lua
+++ init.lua
@@ -20,15 +20,15 @@ require'lspconfig'.pylsp.setup{
   }
 }
 
-vim.g.clipboard = {
-  name = "xclip",
-  copy = {
-    ["+"] = "xclip -selection clipboard -in",
-    ["*"] = "xclip -selection primary -in",
-  },
-  paste = {
-    ["+"] = "xclip -selection clipboard -out",
-    ["*"] = "xclip -selection primary -out",
-  },
-  cache_enabled = 0,
-}
+-- vim.g.clipboard = {
+--   name = "xclip",
+--   copy = {
+--     ["+"] = "xclip -selection clipboard -in",
+--     ["*"] = "xclip -selection primary -in",
+--   },
+--   paste = {
+--     ["+"] = "xclip -selection clipboard -out",
+--     ["*"] = "xclip -selection primary -out",
+--   },
+--   cache_enabled = 0,
+-- }
